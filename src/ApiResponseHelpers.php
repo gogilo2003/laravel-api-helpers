1110e255d13cc87c4bfa5f9a342331f555e531a0
Ogilo G.D. Ouma <gogilo2003@hotmail.com>
2021-10-25 19:14:52 +0300

    Added authSuccess helper function

--- src/ApiResponseHelpers.php
+++ src/ApiResponseHelpers.php
@@ -48,10 +48,24 @@ trait ApiResponseHelpers
         ], 202);
     }
 
+    public function authSuccess($token, $user = null)
+    {
+        $data = [
+            'success' => true,
+            'message' => 'Authentication successful',
+            'access_token' => $token,
+        ];
+
+        if ($user) {
+            $data['user'] = $user;
+        }
+        return response()->json($data, 200);
+    }
+
     public function respondWithSuccess($message, $code = 200)
     {
         return response()->json([
-            'success' => true, 
+            'success' => true,
             'message' => $message
         ], $code);
     }
@@ -59,7 +73,7 @@ trait ApiResponseHelpers
     public function respondWithError($message, $code = 400)
     {
         return response()->json([
-            'success' => false, 
+            'success' => false,
             'message' => $message
         ], $code);
     }
